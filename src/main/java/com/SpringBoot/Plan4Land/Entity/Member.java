56fac3292dc54424f186437c4c1f4ae3c5e3fa14
BeigeSeason <kima606@gmail.com>
2025-01-03 11:27:33 +0900

    2025.01.03 로그인 시 아이디 사용, 전화번호 컬럼 삭제

--- src/main/java/com/SpringBoot/Plan4Land/Entity/Member.java
+++ src/main/java/com/SpringBoot/Plan4Land/Entity/Member.java
@@ -19,6 +19,8 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long uid;
 
+    private String id;
+
     private String email;
 
     private String password;
@@ -26,8 +28,6 @@ public class Member {
     private String name;
 
     private String nickname;
-
-    private String phone;
 
     private String profileImg;
 
