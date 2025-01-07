8058b60bdf3677ad892c2219a6b4a53a139cb1f2
BeigeSeason <kima606@gmail.com>
2025-01-07 10:53:53 +0900

    2025.01.07 강휘 UserDetail 생성 시 권한정보 설정

--- src/main/java/com/SpringBoot/Plan4Land/Service/CustomUserDetailService.java
+++ src/main/java/com/SpringBoot/Plan4Land/Service/CustomUserDetailService.java
@@ -3,13 +3,16 @@ package com.SpringBoot.Plan4Land.Service;
 import com.SpringBoot.Plan4Land.Entity.Member;
 import com.SpringBoot.Plan4Land.Repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class CustomUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
         Member member = memberRepository.findById(id).orElseThrow(()
                 -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + id));
-        return new User(member.getId(), member.getPassword(), new ArrayList<>());
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(member.getRole().name()));
+        return new User(member.getEmail(), member.getPassword(), authorities);
     }
 }
