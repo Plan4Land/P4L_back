654176c83308486bdd3b3ad07b35fd40b2c0b1ed
MyNameIsYGKim <kelen536@gmail.com>
2025-01-07 12:14:49 +0900

    250107 김연규(회원 기능 작성)

---
+++ src/main/java/com/SpringBoot/Plan4Land/Controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.SpringBoot.Plan4Land.Controller;
+
+import com.SpringBoot.Plan4Land.DTO.MemberReqDto;
+import com.SpringBoot.Plan4Land.DTO.MemberResDto;
+import com.SpringBoot.Plan4Land.Service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    // 전체 회원 조회
+    @GetMapping("/list")
+    public ResponseEntity<List<MemberResDto>> memberAllList() {
+        List<MemberResDto> list = memberService.getMemberAllList();
+        return ResponseEntity.ok(list);
+    }
+    // 회원 상세 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<MemberResDto> memberDetail(@PathVariable String userId) {
+        MemberResDto memberResDto = memberService.getMemberDetail(userId);
+        return ResponseEntity.ok(memberResDto);
+    }
+    // 회원 정보 수정
+    @PutMapping("/update")
+    public ResponseEntity<Boolean> memberUpdate(@RequestBody MemberReqDto memberReqDto) {
+        boolean isSuccess = memberService.updateMember(memberReqDto);
+        return ResponseEntity.ok(isSuccess);
+    }
+    // 회원 정보 삭제
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Boolean> memberDelete(@PathVariable String userId) {
+        boolean isSuccess = memberService.deleteMember(userId);
+        return ResponseEntity.ok(isSuccess);
+    }
+}
--- src/main/java/com/SpringBoot/Plan4Land/Service/MemberService.java
+++ src/main/java/com/SpringBoot/Plan4Land/Service/MemberService.java
@@ -1,4 +1,70 @@
 package com.SpringBoot.Plan4Land.Service;
 
+import com.SpringBoot.Plan4Land.DTO.MemberReqDto;
+import com.SpringBoot.Plan4Land.DTO.MemberResDto;
+import com.SpringBoot.Plan4Land.Entity.Member;
+import com.SpringBoot.Plan4Land.Repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@AllArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // 회원 전체 조회
+    public List<MemberResDto> getMemberAllList() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberResDto> memberResDtoList = new ArrayList<>();
+        for (Member member : members) {
+            memberResDtoList.add(convertEntityToDto(member));
+        }
+        return memberResDtoList;
+    }
+
+    // 회원 상세 조회
+    public MemberResDto getMemberDetail(String userId) {
+        Member member = memberRepository.findById(userId).orElseThrow(()->new RuntimeException("해당 회원이 존재하지 않습니다."));
+        return convertEntityToDto(member);
+    }
+
+    // 회원 수정
+    public boolean updateMember(MemberReqDto memberReqDto) {
+        try {
+            Member member = memberRepository.findById(memberReqDto.getId())
+                    .orElseThrow(()->new RuntimeException("해당 회원이 존재하지 않습니다."));
+
+            return true;
+        } catch (Exception e) {
+            log.error("회원정보 수정 : {}", e.getMessage());
+            return false;
+        }
+    }
+
+    // 회원 삭제
+    public boolean deleteMember(String userId) {
+        try {
+            Member member = memberRepository.findById(userId).orElseThrow(()->new RuntimeException("해당 회원이 존재하지 않습니다."));
+            memberRepository.delete(member);
+            return true;
+        } catch (Exception e) {
+            log.error("회원 삭제에 실패했습니다. : {}", e.getMessage());
+            return false;
+        }
+    }
+
+    // Member Entity => MemberResDto 변환
+    private MemberResDto convertEntityToDto(Member member) {
+        MemberResDto memberResDto = new MemberResDto();
+        memberResDto.setEmail(member.getEmail());
+        memberResDto.setName(member.getName());
+        memberResDto.setImgPath(member.getProfileImg());
+        memberResDto.setRegDate(member.getSignupDate());
+        return memberResDto;
+    }
 }
