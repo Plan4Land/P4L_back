5783e9b8f6420b68f802d5f3cefe6c45151225e3
BeigeSeason <kima606@gmail.com>
2025-01-08 17:09:11 +0900

    2025.01.08 강휘 isPublic JsonProperty 어노테이션 추가

--- src/main/java/com/SpringBoot/Plan4Land/DTO/PlannerReqDto.java
+++ src/main/java/com/SpringBoot/Plan4Land/DTO/PlannerReqDto.java
@@ -1,5 +1,6 @@
 package com.SpringBoot.Plan4Land.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,5 +17,7 @@ public class PlannerReqDto {
     private String area;
     private String subArea;
     private String thumbnail;
+
+    @JsonProperty("isPublic")
     private boolean isPublic;
 }
